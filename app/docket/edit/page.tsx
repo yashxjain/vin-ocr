2652fc079501a745bfaa051ee5527d0fad771541
yashxjain <jyash5332@gmail.com>
2026-02-19 16:29:41 +0530

    19-02-2026

--- app/docket/edit/page.tsx
+++ app/docket/edit/page.tsx
@@ -1,7 +1,16 @@
 // app/docket/edit/page.tsx
+import { Suspense } from 'react';
 import DocketEditContent from './DocketEditContent';
-
+import LoadingSpinner from '@/components/LoadingSpinner';
+import DocketViewContent from '../view/DocketViewContent';
 export default function EditDocketPage() {
   // This runs on the server at build time, but just returns a component
-  return <DocketEditContent />;
+    return (
+        <Suspense fallback={<LoadingSpinner />}>
+
+            <DocketEditContent />;
+        </Suspense>
+    )
 }
+
+
